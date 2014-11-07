e41eb3fb8af827f9c973893b5b68c814e0fe58a6
marcin <marcin@arch-vm>
2014-11-07 13:21:27 +0000

    new test

--- HMM/test/tsim.py
+++ HMM/test/tsim.py
@@ -3,14 +3,19 @@
 import random
 import dendropy
 from dendropy import treesim
+import sys
+import os
+import subprocess
+import shutil
+import re
 
 #print(tree.as_newick_string())
 #print(tree.as_string('newick'))
 
 class TreeGenerator:
-    def __init__(self, maxNumber=20):
+    def __init__(self):
         self.fullTaxonSet = []
-        for i in range(maxNumber):
+        for i in range(100):
             self.fullTaxonSet.append('S'+str(i))
 
     def rescaleTree(self, tree, factor):
@@ -18,15 +23,15 @@ class TreeGenerator:
         for i, n in enumerate(nds):
             n.edge_length = random.gauss(n.edge_length*factor, n.edge_length*0.1*factor) 
 
-    def getTree(self, size, minTotalLength=100):
-        tree = treesim.pop_gen_tree(taxon_set=dendropy.TaxonSet(self.fullTaxonSet[0:size]))
+    def getTree(self, size, birthParam):
+        tree = treesim.birth_death(birth_rate=birthParam, death_rate=0, taxon_set=dendropy.TaxonSet(self.fullTaxonSet[0:size]))
         tree.deroot()
-        if tree.length() < minTotalLength :
-            self.rescaleTree(tree,minTotalLength/tree.length())
+        #randomize slightly
+        self.rescaleTree(tree,1.0)
         return tree
 
 class HmmDistanceGenerator:
-    def __init__(self,sequenceLength, replicatesNo, modelName):
+    def __init__(self,numberTaxa, sequenceLength, replicatesNo, modelName):
         #sequence length for indelible
         self.seq_len = int(sequenceLength)
         #number of replicates for indelible
@@ -34,9 +39,11 @@ class HmmDistanceGenerator:
         #model name 
         self.model = modelName
         
+        self.taxaNo = numberTaxa
+        
         self.treegen = TreeGenerator()
         
-        #indicates how many distance steps to generate
+        #indicates how many birth rate steps to generate
         self.steps = 15
         self.paml_binary = 'baseml'
         if (self.model == 'LG'):
@@ -44,7 +51,6 @@ class HmmDistanceGenerator:
         self.indelible_binary = 'indelible'
         self.hmm_binary = 'HMMestF1'
         self.hmm_base_params = ['-F','--in']
-        self.hmm_indel_params = ['-i', str(round(self.indel_rate,2)), '0.5'] 
         self.hmm_misc_params = ['-b', '1', '-o', '0', '--bf', '20']
         self.hmm_alpha_params = ['--rateCat', '5', '--initAlpha', '0.75']
         self.hmm_alpha_est_params = ['--estimateAlpha', '1']
@@ -60,94 +66,133 @@ class HmmDistanceGenerator:
         self.phyml_exec = 'phyml'
         self.prank_exec = 'prank'
         self.muscle_exec = 'muscle'
+
+        self.logfile = open('debug_' + str(self.taxaNo) + '.log', 'w')
         
         self.gamma = 'gamma'
 
-        print("HMM analysis for {} steps with {} replicates. Step size : {}".format(self.steps,self.replicates,self.distance_step))
+        print("HMM analysis for {} steps with {} replicates.".format(self.steps,self.replicates))
 
     def getAlpha(self):
-	return random.uniform(0.1,4.0)
+        return random.uniform(0.1,4.0)
+      
     def getLambda(self):
-	return random.gauss(0.03,0.02)
-    def getEpsilon(self)
-	return rendom.uniform(0.25,0.75)
+        return random.gauss(0.03,0.02)
+      
+    def getEpsilon(self):
+        return random.uniform(0.25,0.75)
+
+    def alignMafft(self, fid):
+        curr_file  = self.indelible_output + '_' + str(fid+1) + '.fas' 
+        mafft_file = 'mafft_' + str(fid+1) + '.fas'
+        mfd = open(mafft_file,'w')
+        subprocess.call([self.mafft_exec, curr_file],stdout=mfd,stderr=self.logfile)
+        mfd.close()
+
+    def alignMuscle(self, fid):
+        curr_file  = self.indelible_output + '_' + str(fid+1) + '.fas' 
+        muscle_file = 'muscle_' + str(fid+1) + '.fas'
+        subprocess.call([self.muscle_exec, '-in', curr_file, '-out', muscle_file],stdout=self.logfile,stderr=self.logfile)
+
+
+    def alignPrank(self, fid):
+        curr_file  = self.indelible_output + '_' + str(fid+1) + '.fas' 
+        prank_file = 'prank_' + str(fid+1)
+        subprocess.call([self.prank_exec, '-d='+curr_file, '-o='+prank_file],stdout=self.logfile,stderr=self.logfile)
+    
+    def alignBatch(self, count):
+        for i in range(count):
+            self.alignMafft(i)
+            self.alignMuscle(i)
+            #self.alignPrank(i)
+
+    def runHMMbatch(self, count,filed,full):
+        threads = []
+        for i in range(count):
+            clean_name = self.indelible_output + '_' + str(i+1) + '.fas'
+            t = Thread(target=self.callHMM, args=(self.hmm_binary, clean_name,filed,full,))
+            threads.append(t)
+            t.start()
+        for th in threads:
+            th.join()
 
     def run(self):
         self.simulate(self.steps,self.replicates,self.model);
-        self.analyze(self.steps,self.replicates,self.model,False);
+        self.analyze(self.steps,self.replicates,self.model);
 
     def simulate(self, s,r,modelname):
-	
-	    ifile = open(self.file_prefix+modelname+self.gamma+self.file_suffix, 'r')
-	    ictl_template = ifile.read()
-	    ifile.close()
-	
-	
-	    while s > 0:
-		
-		tree = self.treegen.getTree(5,5)
-		ofile = open('control.txt', 'w');
-		
-		templateDict = {'alpha' : round(self.getAlpha(),3), 'epsilon' : round(self.getEpsilon(),3), 'lambda' : round(self.getLambda(),3), {newick} : tree.as_newick_string(), 'output' : self.indelible_output, 'length' : self.seq_len, 'replicates' : r}
-		newvals = list(self.proportions);
-		
-		for i in range(len(newvals)):
-			newvals[i] = round(newvals[i] * current_dist/20.0,3);
-		distancesDict = dict(zip(self.keys, newvals))
-		templateDict.update(distancesDict)
+    
+        ifile = open(self.file_prefix+modelname+self.gamma+self.file_suffix, 'r')
+        ictl_template = ifile.read()
+        ifile.close()
+    
+    
+        while s > 0:
+        
+            birth_rate = 0.1 * s
+        
+            tree = self.treegen.getTree(self.taxaNo,birth_rate)
+            ofile = open('control.txt', 'w');
 
-		ofile.write(ictl_template.format(**templateDict))
-		ofile.close()
-		#mkdir GTR + indelible + distance 
-		current_dir = str(self.seq_len) + '_' + str(round(self.indel_rate,2)) + '_Indelible_' + str(r) + '_' + self.model_suffix + '_' + str(round(current_dist,1)) + self.gamma
-		os.mkdir(current_dir)
-		shutil.copy('control.txt',current_dir) 
-		shutil.copy('star.trees',current_dir) 
-		os.chdir(current_dir)
-		#execute indelible
-		subprocess.call('indelible',stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
-		#alignBatch(replicates)
-		#runHMMbatch(replicates)
-		#runPaml(replicates,pml_template)
-		#go back
-		os.chdir('..');
-		current_dist += self.distance_step
-		s -= 1
+            p1 = re.compile('(\(.*\)).*')
+            m1 = p1.search(tree.as_newick_string() )
+            
+            newicktext = m1.group(1) 
+        
+            templateDict = {'alpha' : round(self.getAlpha(),3), 'epsilon' : round(self.getEpsilon(),3), 'lambda' : round(self.getLambda(),3), 'newick' : newicktext, 'output' : self.indelible_output, 'length' : self.seq_len, 'replicates' : r}
+
+            ofile.write(ictl_template.format(**templateDict))
+            ofile.close()
+            #mkdir GTR + indelible + distance 
+            current_dir = str(self.seq_len) + '_' + str(round(birth_rate,1)) + '_Indelible_' + str(r) + '_' + self.model_suffix
+            os.mkdir(current_dir)
+            shutil.copy('control.txt',current_dir) 
+            #shutil.copy('star.trees',current_dir) 
+            os.chdir(current_dir)
+            #execute indelible
+            subprocess.call('indelible',stdout=self.logfile,stderr=self.logfile)
+            #alignBatch(replicates)
+            #runHMMbatch(replicates)
+            #runPaml(replicates,pml_template)
+            #go back
+            os.chdir('..');
+            s -= 1
     
     
-      def analyze(self, s,r,model,onlyPaml):
-	  pfl = open(self.paml_binary + self.gamma + '.paml', 'r');
-	  if (not onlyPaml):
-	      outfile_all = open(self.model + '_' + 'hmm_' + str(self.seq_len) + '_' + str(round(self.indel_rate,2))+ '_' + str(r) +'all' + self.gamma,'w',1)
-	      outfile_ltd = open(self.model + '_' + 'hmm_' + str(self.seq_len) + '_' + str(round(self.indel_rate,2))+ '_' + str(r) +'ltd' + self.gamma,'w',1)
-	  pml_template = pfl.read()
-	  pfl.close()
+    def analyze(self, s,r,model):
+      #pfl = open(self.paml_binary + self.gamma + '.paml', 'r');
+      #if (not onlyPaml):
+      #    outfile_all = open(self.model + '_' + 'hmm_' + str(self.seq_len) + '_' + str(round(self.indel_rate,2))+ '_' + str(r) +'all' + self.gamma,'w',1)
+      #    outfile_ltd = open(self.model + '_' + 'hmm_' + str(self.seq_len) + '_' + str(round(self.indel_rate,2))+ '_' + str(r) +'ltd' + self.gamma,'w',1)
+      #pml_template = pfl.read()
+      #pfl.close()
       
-	  current_dist = self.distance_step
-      
-	  while s > 0:
-	      print("Calculation step {}".format(round(current_dist,1)))
-	      if (not onlyPaml):
-		  outfile_all.write("Analysis step {} \n".format(round(current_dist,1)))
-		  outfile_ltd.write("Analysis step {} \n".format(round(current_dist,1)))
-		  outfile_all.flush()
-		  outfile_ltd.flush()
-	      current_dir =str(self.seq_len) + '_' + str(round(self.indel_rate,2)) + '_Indelible_' + str(r) + '_' + self.model_suffix + '_' + str(round(current_dist,1)) + self.gamma 
-	      shutil.copy('star.trees',current_dir) 
-	      os.chdir(current_dir)
-	      #execute indelible
-	      if (not onlyPaml):
-		  self.runHMMbatch(r,outfile_all,True)
-		  self.runHMMbatch(r,outfile_ltd,False)
-	      self.alignBatch(r)
-	      self.runPaml(r,pml_template)
-	      #go back
-	      os.chdir('..');
-	      current_dist += self.distance_step
-	      s -= 1
-	  if (not onlyPaml):
-	      outfile_all.close()
-	      outfile_ltd.close()
+        while s > 0:
+            birth_rate = 0.1 * s
+            print("Calculation step {}".format(round(birth_rate,1)))
+            current_dir = str(self.seq_len) + '_' + str(round(birth_rate,1)) + '_Indelible_' + str(r) + '_' + self.model_suffix
+            #shutil.copy('star.trees',current_dir) 
+            os.chdir(current_dir)
+            #execute indelible
+            #self.runHMMbatch(r,outfile_all,True)
+            #self.runHMMbatch(r,outfile_ltd,False)
+            self.alignBatch(r)
+            #self.runPaml(r,pml_template)
+            #go back
+            os.chdir('..');
+            s -= 1
+      #if (not onlyPaml):
+      #    outfile_all.close()
+      #    outfile_ltd.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print('Usage: ' + sys.argv[0] + ' numberTaxa seqence_length replicate_no model')
+        raise SystemExit(1)
+
+    runner = HmmDistanceGenerator(int(sys.argv[1]), sys.argv[2], sys.argv[3], sys.argv[4])
+    runner.run()
+
 
 
 
